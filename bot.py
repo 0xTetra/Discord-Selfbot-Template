feda6d0b57f954db61489f248cdf75da5b88b337
0xTetra <74330277+0xTetra@users.noreply.github.com>
2020-11-12 16:14:35 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 """
 This file will register the bot, it's prefix, load all of the commands, then run it using your discord account token.
 To get your discord token, open developer tools (F12)
-Storage -> Local Storage -> CTRL + F5 to refresh -> Scroll to the bottom and copy the "token" parameter.
+Storage -> Local Storage -> CTRL + F5 to refresh -> Scroll to the bottom and copy the "token" value.
 
 If you have any questions regarding this template, contact me via Discord. - 0xTetra#4357
 """
